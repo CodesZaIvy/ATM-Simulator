5fb28c9548c875c31db46c9fe2da5185f52dce83
CodesZaIvy <mosetiivy@gmail.com>
2024-07-01 13:16:50 +0300

    edit pom.xml
    editted the artifactId to suit specifications

---
+++ src/main/java/com/techie/bankmanagementsystem/Main.java
@@ -0,0 +1,7 @@
+package com.techie.bankmanagementsystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Well,this is a done project already!");
+    }
+}
